0fbcad5349941061bc582ee995979ede4c8097fb
Zachary Durr <zdurr22@gmail.com>
2020-10-01 16:09:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,14 +2,11 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  arr = []
-  arr << badge_maker(attendees[0])
-  arr << badge_maker(attendees[1])
-  arr << badge_maker(attendees[2])
-  arr << badge_maker(attendees[3])
-  arr << badge_maker(attendees[4])
-  arr << badge_maker(attendees[5])
-  arr << badge_maker(attendees[6])
-  arr
+def batch_badge_creator(speakers)
+  badge_messages = []
+  speakers.each do |speaker|
+    message = badge_maker(speaker)
+    badge_messages << message
+  end
+  badge_messages
 end
